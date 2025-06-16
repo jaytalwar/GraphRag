f032b89934157f292c93421aa0caeb6d4c7bb38c
harshikash29 <mgpsharshika6250@gmail.com>
2025-06-16 16:19:07 +0530

    Intialised task -2 by adding __init__(),close(),load_json_files(),create_node() in knowledge_graph.py .Also created queries.json.

--- common/knowledge_graph.py
+++ common/knowledge_graph.py
@@ -1,1 +1,37 @@
+import os
+import json
+from neo4j import GraphDatabase
+ 
 class KnowledgeGraph:
+    def __init__(self, uri, username, password, data_directory, text_processor,database="neo4j"):
+        """ Initializes a connection to a Neo4j database and sets up paths and processors for handling data and text processing."""
+        self.driver = GraphDatabase.driver(uri, auth=(username, password))
+        self.data_directory = data_directory
+        self.text_processor = text_processor
+        self.database = database
+        self.json_nodes = {}
+        with open("queries.json","r") as f:
+            self.queries=json.load(f)
+ 
+    def close(self):
+        """ Shuts down the connection to the Neo4j database by closing the driver."""
+        self.driver.close()
+ 
+    def load_json_files(self):
+        """ Reads all .json files from the specified data directory, loads their contents, and stores each file's data. """
+        for filename in os.listdir(self.data_directory):
+            if filename.endswith(".json"):
+                path = os.path.join(self.data_directory, filename)
+                with open(path, 'r', encoding='utf-8') as f:
+                    data = json.load(f)
+                    self.json_nodes[data['path']] = data
+ 
+    def create_node(self, tx, node):
+        """ Adds a unique File or Directory node to Neo4j with its path, name, content, and generated embedding."""
+        label = "Directory" if node['type'] == 'directory' else "File"
+        embedding = self.text_processor.generate_embedding(node.get('content', ''))
+        query=self.queries["create_node"].replace("{label}",label)
+        tx.run(query,path=node['path'],name=node['name'],
+                content=node.get('content',''),embedding=embedding)
+ 
+   
